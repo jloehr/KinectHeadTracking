a5b024f2d881ef95df562878df1890ba824dcb2d
Julian Löhr <julian.loehr@outlook.com>
2016-07-08 17:44:20 +0200

    Add basic Run Implementation

--- KinectFaceTracking/KinectFaceTracking.cpp
+++ KinectFaceTracking/KinectFaceTracking.cpp
@@ -3,3 +3,39 @@
 
 #include "stdafx.h"
 #include "KinectFaceTracking.h"
+
+KinectFaceTracking::KinectFaceTracking(_In_ HINSTANCE Instance)
+	:Instance(Instance)
+{
+}
+
+int KinectFaceTracking::Run(_In_ int nCmdShow)
+{
+	Initialize(nCmdShow);
+
+	// Loop
+	MSG Message = {};
+	do {
+		while (PeekMessage(&Message, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&Message);
+			DispatchMessage(&Message);
+		}
+
+		// Update
+		// Render
+
+	} while (Message.message != WM_QUIT);
+
+	Destroy();
+
+	return static_cast<int>(Message.wParam);
+}
+
+void KinectFaceTracking::Initialize(_In_ int nCmdShow)
+{
+}
+
+void KinectFaceTracking::Destroy()
+{
+}
--- KinectFaceTracking/KinectFaceTracking.h
+++ KinectFaceTracking/KinectFaceTracking.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include "resource.h"
+
+
+class KinectFaceTracking
+{
+public:
+	KinectFaceTracking(_In_ HINSTANCE Instance);
+
+	int Run(_In_ int nCmdShow);
+
+private:
+	HINSTANCE Instance;
+
+	void Initialize(_In_ int nCmdShow);
+	void Destroy();
+};
